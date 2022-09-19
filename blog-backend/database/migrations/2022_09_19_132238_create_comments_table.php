7f3cf57dab195829fe5ca2ec531a68026da93173
“abhishekpanda626” <abhishek.panda@nettantra.net>
2022-09-19 18:56:17 +0530

    Add Comment and Post table

---
+++ blog-backend/database/migrations/2022_09_19_132238_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string("comment");
+            $table->foreignId("user_id")->nullable();
+            $table->foreignId("post_id")->nullable();
+            $table->string("file_path")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
